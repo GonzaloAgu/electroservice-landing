9616559225886716e7f683364144ae7d3611e30f
GonzaloAgu <gonzalo.ag88@gmail.com>
2024-11-01 00:09:43 -0300

    posicion absoluta en el fondo

--- src/components/Circuits.jsx
+++ src/components/Circuits.jsx
@@ -8,6 +8,7 @@ export default function Circuits() {
       viewBox="0 0 2400 1600"
       version="1.1"
       xmlns="http://www.w3.org/2000/svg"
+      style={{ position: "absolute"}}
     >
       <defs>
         <linearGradient id="linear" x1="0%" y1="0%" x2="100%" y2="0%">
