924a8079dd9eee1f499a2e4420be65a0b9509382
GonzaloAgu <gonzalo.ag88@gmail.com>
2024-11-01 00:50:42 -0300

    seccion que ofrecemos

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,6 @@ const App = () => {
       <About data={landingPageData.About} />
       <Gallery data={landingPageData.Gallery} />
       <Testimonials data={landingPageData.Testimonials} />
-      <Team data={landingPageData.Team} />
       <Contact data={landingPageData.Contact} />
     </div>
   );
--- src/components/features.jsx
+++ src/components/features.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Features = (props) => {
   return (
-    <div id="features"  className="text-center" style={{paddingTop: "70px", marginTop: "-70px"}}>
+    <div id="features"  className="text-center" style={{paddingTop: "70px", marginTop: "-70px", paddingBottom: "30px"}}>
       <div className="container">
         <div className="col-md-10 col-md-offset-1 section-title">
           <h2 style={{paddingTop: "40px"}}>¿Qué ofrecemos?</h2>
@@ -10,7 +10,7 @@ export const Features = (props) => {
         <div className="row">
           {props.data
             ? props.data.map((d, i) => (
-                <div key={`${d.title}-${i}`} className="col-xs-6 col-md-3">
+                <div key={`${d.title}-${i}`} className="col-xs-6 col-md-4">
                   {" "}
                   <i className={d.icon}></i>
                   <h3>{d.title}</h3>
