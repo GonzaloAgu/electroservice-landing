0fe1efcfec846939153f5c07da01fad6ed6174b1
GonzaloAgu <gonzalo.ag88@gmail.com>
2024-10-31 23:53:40 -0300

    fondo de circuitos animado

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import { Contact } from "./components/contact";
 import JsonData from "./data/data.json";
 import SmoothScroll from "smooth-scroll";
 import "./App.css";
+import Circuits from "./components/Circuits";
 
 export const scroll = new SmoothScroll('a[href*="#"]', {
   speed: 1000,
@@ -25,6 +26,7 @@ const App = () => {
 
   return (
     <div>
+      <Circuits/>
       <Navigation />
       <Header data={landingPageData.Header} />
       <Features data={landingPageData.Features} />
