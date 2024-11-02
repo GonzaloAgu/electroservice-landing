343139f7a976a4bd32014e66b2052944a9d9a3f0
GonzaloAgu <gonzalo.ag88@gmail.com>
2024-11-02 14:17:50 -0300

    improved responsiveness in about

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -4,17 +4,17 @@ export const About = (props) => {
   return (
     <div id="about" className="section">
       <div className="container">
-        <div className="row">
+        <div className="row justify-content-center">
           <div className="col-xs-12 col-md-6">
             {" "}
-            <img src="img/front.jpg" className="img-responsive" alt="" />{" "}
+            <img src="img/front.jpg" className="img-responsive" alt="Imagen frontal del local Electroservice" />{" "}
           </div>
-          <div className="col-xs-12 col-md-6">
+          <div className="col-xs-12 col-md-6 d-flex justify-content-center">
             <div className="about-text">
               <h2>Sobre Nosotros</h2>
               <p>{props.data ? props.data.paragraph : "loading..."}</p>
               <h3>¿Qué reparamos?</h3>
-              <div className="list-style">
+              <div className="row justify-content-center">
                 <div className="col-lg-6 col-sm-6 col-xs-12">
                   <ul>
                     {props.data
