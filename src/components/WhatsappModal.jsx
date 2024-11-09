212be2e330f179a43534a59ccdfa669766175fcf
GonzaloAgu <gonzalo.ag88@gmail.com>
2024-11-09 20:49:35 -0300

    unidad de ancho

--- src/components/WhatsappModal.jsx
+++ src/components/WhatsappModal.jsx
@@ -7,7 +7,7 @@ export default function WhatsappModal({show, onClose}) {
             <button className="close-btn" onClick={onClose}><i className="fa fa-times" aria-hidden="true"></i></button>
             <div>
                 <h3>Consulta rápida por WhatsApp</h3>
-                <p>Este formulario generará un mensaje para enviarnos (se abrirá WhatsApp con el mensaje armado).</p>
+                <p>Este formulario generará un mensaje para enviarnos (se abrirá WhatsApp con el mensaje armado y listo para enviar).</p>
                 <p>Si bien los campos son opcionales, ingrese toda la información posible para obtener una mejor respuesta.</p>
             </div>
             <div className="form">
