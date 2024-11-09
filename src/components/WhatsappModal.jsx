b4e643c95d91b6e4e4cda712d06c454eb4a22c5c
GonzaloAgu <gonzalo.ag88@gmail.com>
2024-11-09 20:38:51 -0300

    modal responsive

--- src/components/WhatsappModal.jsx
+++ src/components/WhatsappModal.jsx
@@ -4,10 +4,10 @@ export default function WhatsappModal({show, onClose}) {
         <>
         {show && <div className="mask"></div>}
         <div className={`modal-whatsapp ${!show && 'd-none'}`}>
-            <button className="close-btn" onClick={onClose}>X</button>
+            <button className="close-btn" onClick={onClose}><i className="fa fa-times" aria-hidden="true"></i></button>
             <div>
                 <h3>Consulta rápida por WhatsApp</h3>
-                <p>Este formulario generará un mensaje para enviarnos por WhatsApp (se abrirá WhatsApp con el mensaje armado).</p>
+                <p>Este formulario generará un mensaje para enviarnos (se abrirá WhatsApp con el mensaje armado).</p>
                 <p>Si bien los campos son opcionales, ingrese toda la información posible para obtener una mejor respuesta.</p>
             </div>
             <div className="form">
@@ -17,6 +17,10 @@ export default function WhatsappModal({show, onClose}) {
                 <input type="text" placeholder="ej: televisor"></input>
                 <label>Marca y modelo</label>
                 <input type="text" placeholder="ej: Samsung T4300"></input>
+
+                <label>Consulta</label>
+                <textarea placeholder="ej: Hola, ¿ya está mi producto disponible para su retiro?" rows={10}></textarea>
+                <button className="btn btn-success send-btn"><i className="fa fa-whatsapp" aria-hidden="true" style={{fontSize: '2rem'}}></i> Enviar</button>
             </div>
         </div>
         </>
